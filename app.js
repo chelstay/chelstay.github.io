215456433e2db7a908ad3445585370cbaaff5fe9
Chelsea Scriven <chelseascriven@Chelseas-MacBook-Pro.local>
2020-07-03 16:59:53 -0700

    1st commit website portfolio

---
+++ app.js
@@ -0,0 +1,15 @@
+// jQuery Smooth scroll
+
+$('a').on('click', function(e) {
+  if (this.hash !== ''); {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
