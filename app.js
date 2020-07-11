4aa379da79f5739f3978f73201df47d8c2d25c46
Chelsea Scriven <chelseascriven@Chelseas-MacBook-Pro.local>
2020-07-11 16:32:28 -0700

    add fade effect

--- app.js
+++ app.js
@@ -15,6 +15,45 @@ $('a').on('click', function(e) {
 });
 
 
+
+
+// Fade in
+
+$(document).ready(function () {
+    $('.toshow1').fadeIn(2000);
+    $('.toshow1').slideDown("slow");
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+
+$(document).ready(function () {
+    $('.toshow2').fadeIn(3000);
+    $('.toshow2').slideDown("slow");
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+
+$(document).ready(function () {
+    $('.toshow3').fadeIn(4000);
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+
+            var bottom_of_element = $(this).offset().top;
+            var bottom_of_window = $(window).scrollTop() + ($(window).height() / 2);
+
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},800);
+            }
+
+        });
+    });
+});
+
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
