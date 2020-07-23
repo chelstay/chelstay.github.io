eb6d1be378969d543261ab4108d916f50955a765
Chelsea Scriven <chelseascriven@Chelseas-MacBook-Pro.local>
2020-07-22 18:25:32 -0700

    display text

--- app.js
+++ app.js
@@ -45,30 +45,19 @@ document.getElementById("showHide").onclick = function() {
 }
 
 
-(function() {
-  var elements;
-  var windowHeight;
 
-  function init() {
-    elements = document.querySelectorAll('.hidden');
-    windowHeight = window.innerHeight;
-  }
+$(document).on("scroll", function () {
+var pageTop = $(document).scrollTop()
+var pageBottom = pageTop + $(window).height()
+var tags = $("section")
 
-  function checkPosition() {
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var positionFromTop = elements[i].getBoundingClientRect().top;
+for (var i = 0; i < tags.length; i++) {
+var tag = tags[i]
 
-      if (positionFromTop - windowHeight <= 0) {
-        element.classList.add('fade-in-element');
-        element.classList.remove('hidden');
-      }
-    }
-  }
-
-  window.addEventListener('scroll', checkPosition);
-  window.addEventListener('resize', init);
-
-  init();
-  checkPosition();
-})();
+if ($(tag).position().top < pageBottom) {
+$(tag).addClass("visible")
+} else {
+  $(tag).removeClass("visible")
+}
+}
+})
