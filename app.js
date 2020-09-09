b9d4b347ecce169b48b0df6276a1c5243fb49e94
chelstay <65387006+chelstay@users.noreply.github.com>
2020-09-09 16:18:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -62,14 +62,4 @@ $(tag).addClass("visible")
 }
 })
 
-$(document).ready(function () {
-    $(document).click(
-        function (event) {
-            var target = $(event.target);
-            var _mobileMenuOpen = $(".navbar-collapse").hasClass("show");
-            if (_mobileMenuOpen === true && !target.hasClass("navbar-toggler")) {
-                $("button.navbar-toggler").click();
-            }
-        }
-    );
-});
+
